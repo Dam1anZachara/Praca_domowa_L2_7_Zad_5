1e5b04f3b5d0d3f2e1ff72fece84fa7f03189136
dzachara <dzachara@ZACHARA-D>
2021-12-16 13:34:52 +0100

    Add project files.

---
+++ L2_7_Zad_5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace L2_7_Zad_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ten program sprawdzi czy w Twój wiek uprawnia Cię do ubiegania się o dany urząd.");
+            Console.Write("Podaj swój wiek: ");
+            byte.TryParse(Console.ReadLine(), out byte userAge);
+
+            if (userAge >= 21)
+            {
+                Console.WriteLine("Możesz zostać posłem.");
+                if(userAge >= 25)
+                {
+                    Console.WriteLine("Możesz zostać premierem.");
+                    if (userAge >= 30)
+                    {
+                        Console.WriteLine("Możesz zostać senatorem.");
+                        if (userAge >= 35)
+                        {
+                            Console.WriteLine("Możesz zostać prezydentem.");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Jesteś za młody aby ubuegać się o urząd.");
+            }
+        }
+    }
+}
